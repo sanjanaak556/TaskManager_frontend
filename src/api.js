89cbccedb6224411adb190948355c6180f67345f
Sanjana <sanjanaak556@gmail.com>
2025-11-06 01:07:16 +0530

    updated api.js

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "http://localhost:8000/api", //backend URL
+  baseURL: "https://tasknest-backend-f676.onrender.com/", //backend URL(render)
 });
 
 // Attach JWT token automatically
